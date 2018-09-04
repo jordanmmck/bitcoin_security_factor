5eae2268ec504aca841ff8debb40f7a6281eebbe
Jordan McKinney <jordanmmck@gmail.com>
2018-09-03 18:47:17 -0700

    all 4 plots working

--- security_factor.py
+++ security_factor.py
@@ -1,15 +1,19 @@
 import os
 import json
 import requests
-import datetime
+import datetime as dt
 import sys
 
 import matplotlib.pyplot as plt
+import matplotlib.dates as md
 import numpy as np
 from scipy import stats
 
 from helper import avg
 
+
+SECONDS_PER_YEAR = 31557600
+EPOCH_2010 = 1262304000
 
 # no data until Aug. 18 2010
 arguments = '?format=json&start=2010-08-18&timespan=10year'
@@ -43,12 +47,14 @@ with open('tx_fees_data.json') as f:
 fee_ratios = []
 rev_ratios = []
 days = []
+unix_days = []
 
-lin_reg_days = []
-lin_reg_fee_ratios = []
+unix_start_time = fee_data[0]['x']
+start_date = dt.datetime.fromtimestamp(unix_start_time)
 
-start_date = datetime.datetime.fromtimestamp(fee_data[0]['x'])
-end_date = start_date + datetime.timedelta(days=365*10)
+end_date = start_date + dt.timedelta(days=365*10)
+unix_end_time = unix_start_time + 10*SECONDS_PER_YEAR
+
 day = start_date
 
 for mcap_dict, rev_dict, fee_dict in zip(mcap_data, rev_data, fee_data):
@@ -58,15 +64,14 @@ for mcap_dict, rev_dict, fee_dict in zip(mcap_data, rev_data, fee_data):
 
     fee_ratio = fee/mcap
     rev_ratio = rev/mcap
-    lin_reg_days.append(float(mcap_dict['x']))
-    lin_reg_fee_ratios.append(fee_ratio)
 
     fee_ratios.append(fee_ratio)
     rev_ratios.append(rev_ratio)
 
+    unix_days.append(float(mcap_dict['x']))
     days.append(day)
     # data points exist for every other day only
-    day += datetime.timedelta(days=2)
+    day += dt.timedelta(days=2)
 
 
 avg_fee_ratio = avg(fee_ratios)
@@ -76,8 +81,8 @@ print('Average Daily Security Factor: {0:.7f}'.format(avg_rev_ratio))
 print('Average Daily Fee-Only Security Factor: {0:.9f}'.format(avg_fee_ratio))
 
 
-start_plot = start_date - datetime.timedelta(days=100)
-end_plot = end_date - datetime.timedelta(days=500)
+start_plot = start_date - dt.timedelta(days=100)
+end_plot = end_date - dt.timedelta(days=500)
 
 # Plot revenue security factor (full)
 plt.ylim(ymin=0, ymax=0.0032)
@@ -90,8 +95,8 @@ plt.title('Bitcoin Security Factor')
 plt.legend()
 plt.show()
 
-start_plot = start_date + datetime.timedelta(days=350)
-end_plot = end_date - datetime.timedelta(days=600)
+start_plot = start_date + dt.timedelta(days=350)
+end_plot = end_date - dt.timedelta(days=600)
 
 # Plot revenue security factor (med zoom)
 plt.ylim(ymin=0, ymax=0.0008)
@@ -104,8 +109,8 @@ plt.title('Bitcoin Security Factor')
 plt.legend()
 plt.show()
 
-start_plot = start_date + datetime.timedelta(days=1800)
-end_plot = end_date - datetime.timedelta(days=600)
+start_plot = start_date + dt.timedelta(days=1800)
+end_plot = end_date - dt.timedelta(days=600)
 
 # Plot revenue security factor (close zoom)
 plt.ylim(ymin=0, ymax=0.0003)
@@ -118,38 +123,30 @@ plt.title('Bitcoin Security Factor')
 plt.legend()
 plt.show()
 
-start_plot = start_date - datetime.timedelta(days=100)
-end_plot = end_date - datetime.timedelta(days=600)
+# tx fee security factor
+unix_days = np.array(unix_days, dtype=float)
 
-# Plot fee figure
-plt.plot(days, fee_ratios)
-plt.ylim(ymin=0, ymax=0.00005)
-plt.xlim(xmin=start_plot, xmax=end_plot)
-plt.hlines(avg_fee_ratio, start_plot, end_date, color='orange', linewidth=0.6, linestyle='dashed', label='Daily fee revenue to market cap ratio (all-time average)')
-plt.hlines(0.000001, start_plot, end_date, color='red', linewidth=0.6, linestyle='dashed', label='estimate baseline level')
-plt.xlabel('Date')
-plt.ylabel('Total daily TX fees to market cap ratio')
+# trendline
+slope = 7.9E-15
+y_intercept = -1E-5
+trendline_label = 'Trendline (y = {} * UNIX_TIME + {})'.format(slope, y_intercept)
+trendline = slope * unix_days + y_intercept
+
+plt.plot(unix_days, fee_ratios, 'o', markersize=2, label='Security Factor', color='grey')
+plt.plot(unix_days, trendline, label=trendline_label, color='blue', linestyle='dashed', linewidth=0.8)
+plt.hlines(avg_fee_ratio, unix_start_time, unix_end_time, color='orange', linewidth=0.6, linestyle='dashed', label='Fee security factor average ({:.7f})'.format(avg_fee_ratio))
+
+# human readable labels
+unix_years = [EPOCH_2010 + SECONDS_PER_YEAR*i for i in range(1,10)]
+year_labels = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
+plt.xticks(unix_years, year_labels)
+
+plt.ylim(ymin=-0.000001, ymax=0.00005)
+plt.xlim(xmin=unix_start_time-10000000, xmax=unix_end_time-SECONDS_PER_YEAR)
+
+plt.xlabel('Year')
+plt.ylabel('Daily TX fee security factor')
 plt.title('Bitcoin TX Fee Security Factor')
 plt.legend()
-plt.show()
 
-
-# Plot fee figure w/lin regression
-lin_reg_days = np.array(lin_reg_days, dtype=float)
-lin_reg_fee_ratios = np.array(lin_reg_fee_ratios, dtype=float)
-
-slope, intercept, r_value, p_value, std_err = stats.linregress(lin_reg_days, lin_reg_fee_ratios)
-line = slope*lin_reg_days+intercept
-print(slope, intercept)
-line_formula = 'y = ' + str(slope) + ' x + ' + str(intercept)
-print(line_formula)
-
-plt.plot(lin_reg_days, lin_reg_fee_ratios, 'o', markersize=2, label='tx fee sec ratio')
-plt.plot(lin_reg_days, line, label='optimistic fee projection')
-
-plt.xlabel('Date')
-plt.ylabel('Total daily TX fees to market cap ratio')
-plt.title('Bitcoin TX Fee Security Factor Regression')
-
-plt.legend()
 plt.show()
