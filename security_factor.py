2d23c9c4d57dc44b7264c90c5c81c7b4648d42ae
Jordan McKinney <jordanmmck@gmail.com>
2018-09-09 09:55:59 -0700

    interpolating missing times

--- security_factor.py
+++ security_factor.py
@@ -6,6 +6,7 @@ import time
 import calendar
 import sys
 import math
+import pprint
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
@@ -38,7 +39,7 @@ supply = 0
 blocks = {}
 
 # construct supply schedule
-for block in range(0,700000):
+for block in range(0,7000000):
     try:
         daily_inflation = reward * BLOCKS_PER_DAY / supply
     except ZeroDivisionError:
@@ -55,39 +56,56 @@ for block in range(0,700000):
         reward = reward//2
     supply += reward
 
+# block discovery dates
+# https://en.bitcoin.it/wiki/Controlled_supply
+# https://www.blockchain.com/btc/block-height/540000
+block_dates = [
+    {'block': 0, 'date': '2009-01-03', 'unix_date': None, 'time_step': None},
+    {'block': 52500, 'date': '2010-04-22', 'unix_date': None, 'time_step': None},
+    {'block': 105000, 'date': '2011-01-28', 'unix_date': None, 'time_step': None},
+    {'block': 157500, 'date': '2011-12-14', 'unix_date': None, 'time_step': None},
+    {'block': 210000, 'date': '2012-11-28', 'unix_date': None, 'time_step': None},
+    {'block': 262500, 'date': '2013-10-09', 'unix_date': None, 'time_step': None},
+    {'block': 315000, 'date': '2014-08-11', 'unix_date': None, 'time_step': None},
+    {'block': 367500, 'date': '2015-07-29', 'unix_date': None, 'time_step': None},
+    {'block': 420000, 'date': '2016-07-09', 'unix_date': None, 'time_step': None},
+    {'block': 472500, 'date': '2017-06-23', 'unix_date': None, 'time_step': None},
+    {'block': 525000, 'date': '2018-05-29', 'unix_date': None, 'time_step': None},
+    {'block': 540000, 'date': '2018-09-05', 'unix_date': None, 'time_step': None},
+]
+
+# add unix times
+for block in block_dates:
+    block_date = block['date']
+    block['unix_date'] = calendar.timegm(
+            dt.datetime.strptime(block_date, '%Y-%m-%d').timetuple()
+        )
+
+# interpolate missing block times
+for i, block in enumerate(block_dates):
+    try:
+        time_diff = block_dates[i+1]['unix_date'] - block['unix_date']
+        block_diff = block_dates[i+1]['block'] - block['block']
+        block['time_step'] = time_diff/block_diff
+    except IndexError:
+        block['time_step'] = 10*60
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(block_dates)
+
 # 1. construct simple block to reward and supply map
 # 2. pin known dates to blocks
 # 3. extrapolate missing dates
 # 4. pin fee to every 288th block? None the rest?
 
-# block discovery dates
-# source: https://en.bitcoin.it/wiki/Controlled_supply
-block_dates = {
-    0: '2009-01-03',
-    52500: '2010-04-22',
-    105000: '2011-01-28',
-    157500: '2011-12-14',
-    210000: '2012-11-28',
-    262500: '2013-10-09',
-    315000: '2014-08-11',
-    367500: '2015-07-29',
-    420000: '2016-07-09',
-    472500: '2017-06-23',
-    525000: '2018-05-29'
-}
-
-# add unix times
-for block in block_dates:
-    block_date = block_dates[block]
-    blocks[block]['date'] = block_date
-    blocks[block]['unix_date'] = calendar.timegm(
-            dt.datetime.strptime(block_date, '%Y-%m-%d').timetuple()
-        )
-
-
-# inflations = np.array([d['daily_inflation'] for d in blocks.values()])
-# plt.semilogy(blocks.keys(), inflations)
+inflations = np.array([d['daily_inflation'] for d in blocks.values()])
+# dates = np.array([d['date'] for d in blocks.values()])
+# dates = np.array([d['date'] for d in blocks.values()])
+plt.semilogy(blocks.keys(), inflations)
+# plt.plot(blocks.keys(), inflations)
 # plt.ylim(ymin=1E-14, ymax=1)
+# plt.ylim(ymin=0, ymax=1E-5)
+plt.xlim(xmin=0, xmax=7100000)
 # plt.xlabel('Block Number')
 # plt.ylabel('Daily Security Factor')
-# plt.show()
+plt.show()
